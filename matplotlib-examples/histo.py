3b9a135401516f2f58ca66b3ff261f9b254b9956
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-10-03 20:08:34 +0100

    learning plots

--- matplotlib-examples/histo.py
+++ matplotlib-examples/histo.py
@@ -3,6 +3,13 @@ import numpy as np
 
 x = np.random.normal(0.0,1.0,1000)
 
-plt.hist(x, bins = 20)
+plt.subplot(1,2,1) # 1 row, 2 columns, 1st plot    . Bell shaped curve
+x = np.random.normal(0.0,1.0,10000)
+plt.hist(x)
+
+plt.subplot(1,2,2)
+x = np.random.uniform(-3.0,3.0,10000) # 1 row, 2 columns, 2nd plot
+plt.hist(x)
+
 
 plt.show()
